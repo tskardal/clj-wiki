fd057662c924b22f57950ebb5dcbada0025a803e
Thomas Skardal <thomas.skardal@gmail.com>
2015-01-10 13:36:27 +0100

    Rendering page content from markdown!

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
                  [hiccup "1.0.5"]
                  [clj-tagsoup "0.3.0"]
                  [ring-server "0.3.1"]
-                 [com.novemberain/monger "2.0.0"]]
+                 [com.novemberain/monger "2.0.0"]
+                 [markdown-clj "0.9.62"]]
   :plugins [[lein-ring "0.8.12"]]
   :ring {:handler wiki.handler/app
          :init wiki.handler/init
--- src/wiki/routes/home.clj
+++ src/wiki/routes/home.clj
@@ -2,6 +2,7 @@
   (:require [compojure.core :refer :all]
             [ring.util.response :refer [redirect]]
             [hiccup.form :refer [form-to]]
+            [markdown.core :refer [md-to-html-string]]
             [wiki.views.layout :as layout]
             [wiki.models.pages :as pages]
             [pl.danieljanus.tagsoup :refer [parse-string]]))
@@ -11,8 +12,9 @@
 
 ;; TODO bytt til reagent og lag edit-knapp og textarea for endring av innhold
 (defn- render-content [{content :content :as page}]
-  (let [with-links (clojure.string/replace content #"(\[\[(.+)\]\])" "<a href=\"/$2\">$2</a>")]
-    [:p (parse-string (str "<div>" with-links "</div>"))]))
+  (let [with-links (clojure.string/replace content #"(\[\[(.+)\]\])" "<a href=\"/$2\">$2</a>")
+        md (md-to-html-string with-links)]
+    [:p (parse-string (str "<div>" md "</div>"))]))
 
 (defn render-page [page-name]
   (let [page (pages/find-page page-name)]    
