a7a37e0063d1e08a5d255b63dfb818ddbbcf2a0b
Thomas Skardal <thomas.skardal@gmail.com>
2015-01-15 22:35:01 +0100

    Avoid unncessary query after saving

--- src/wiki/models/pages.clj
+++ src/wiki/models/pages.clj
@@ -8,11 +8,9 @@
 (defn- persist [page]
   (let [conn (mg/connect)
         db (mg/get-db conn "wikidb")
-        oid (ObjectId.)]
-    (mc/update db "pages" {:name (:name page)} {$set page $setOnInsert {:_id oid}} {:upsert true})
-    ;; TODO this extra query feels unncessary
-    (let [{stored-id :_id} (mc/find-one-as-map db "pages" {:name (:name page)})]
-      (store-index (merge {:_id (str stored-id)} page)))))
+        oid (or (:_id page) (ObjectId.))]    
+    (mc/update db "pages" {:name (:name page)} {$set page $setOnInsert {:_id oid}} {:upsert true})    
+    (store-index (merge {:_id (str oid)} page))))
 
 (defn- lookup [name]
   (let [conn (mg/connect)
